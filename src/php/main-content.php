780d779a547ad014898e8c64c14121a64c8742d3
nol4lej <nome.ahumada@outlook.com>
2023-05-09 12:45:00 -0400

    Avances dashboard responsive + navbar

--- src/php/main-content.php
+++ src/php/main-content.php
@@ -8,7 +8,12 @@
 </head>
 <body>
     <main class="container_principal">
-        aaaaaaasddd Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, earum dolore quidem cupiditate nesciunt quis? Magnam culpa, error totam optio recusandae perspiciatis nemo quibusdam ipsa quo! Quam similique enim obcaecati!
+        <div class="last_publications">
+            <h2>Últimos comentarios</h2>
+            <div class="last_publications_table">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dicta tempora consectetur, beatae deleniti sapiente corrupti non fugiat, commodi esse culpa saepe molestiae asperiores unde labore similique sed aspernatur earum!</p>
+            </div>
+        </div>
     </main>
 </body>
 </html>
